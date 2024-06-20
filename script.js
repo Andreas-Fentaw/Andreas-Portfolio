71dbb19f812f3af1ecdd158c4da0e4e1b5fa30f4
neighborhood-karen <andreasfentaw@gmail.com>
2024-06-20 07:20:49 -0400

    asdfghjk

---
+++ script.js
@@ -0,0 +1,51 @@
+let myLink1 = document.querySelector("#myLink1");
+let myLink2 = document.querySelector("#myLink2");
+let myLink3 = document.querySelector("#myLink3");
+let myLink4 = document.querySelector("#myLink4");
+
+//----------------------------------------------------------------------------------------------
+
+myLink1.addEventListener("mouseover", function () {
+    myLink1.style.color = "white";
+    myLink1.style.fontSize = "1.8vw";
+});
+
+myLink1.addEventListener("mouseout", function () {
+    myLink1.style.color = "rgb(76, 76, 76)";
+    myLink1.style.fontSize = "1.4vw";
+});
+
+//----------------------------------------------------------------------------------------------
+
+myLink2.addEventListener("mouseover", function () {
+    myLink2.style.color = "white";
+    myLink2.style.fontSize = "1.8vw";
+});
+
+myLink2.addEventListener("mouseout", function () {
+    myLink2.style.color = "rgb(76, 76, 76)";
+    myLink2.style.fontSize = "1.4vw";
+});
+
+//----------------------------------------------------------------------------------------------
+
+myLink3.addEventListener("mouseover", function () {
+    myLink3.style.color = "white";
+    myLink3.style.fontSize = "1.8vw";
+});
+
+myLink3.addEventListener("mouseout", function () {
+    myLink3.style.color = "rgb(76, 76, 76)";
+    myLink3.style.fontSize = "1.4vw";
+});
+//----------------------------------------------------------------------------------------------
+
+myLink4.addEventListener("mouseover", function () {
+    myLink4.style.color = "white";
+    myLink4.style.fontSize = "1.8vw";
+});
+
+myLink4.addEventListener("mouseout", function () {
+    myLink4.style.color = "rgb(76, 76, 76)";
+    myLink4.style.fontSize = "1.4vw";
+});
