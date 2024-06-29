b01e47c84692a371efca672d496249c968b0becf
neighborhood-karen <96897935+neighborhood-karen@users.noreply.github.com>
2024-06-29 08:25:35 -0700

    my homepage look

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let myLink1 = document.querySelector("#myLink1");
 
+mylinksColor = "white";
+myLinksColorOut = "rgb(157, 157, 157)";
+
 myLink1.addEventListener("mouseover", function () {
-    myLink1.style.color = "black";
+    myLink1.style.color = mylinksColor;
     });
     
 myLink1.addEventListener("mouseout", function () {
-    myLink1.style.color = "rgb(76, 76, 76)";
-
+    myLink1.style.color = myLinksColorOut;
     });
         
 //----------------------------------------------------------------------------------------------
@@ -15,11 +17,11 @@ let myLink2 = document.querySelector("#myLink2");
 
         
 myLink2.addEventListener("mouseover", function () {
-    myLink2.style.color = "black";
+    myLink2.style.color = mylinksColor;
 });
 
 myLink2.addEventListener("mouseout", function () {
-    myLink2.style.color = "rgb(76, 76, 76)";
+    myLink2.style.color = myLinksColorOut;
 
 });
 
@@ -28,26 +30,13 @@ myLink2.addEventListener("mouseout", function () {
 let myLink3 = document.querySelector("#myLink3");
 
 myLink3.addEventListener("mouseover", function () {
-    myLink3.style.color = "black";
+    myLink3.style.color =  mylinksColor;
 });
 
 myLink3.addEventListener("mouseout", function () {
-    myLink3.style.color = "rgb(76, 76, 76)";
+    myLink3.style.color = myLinksColorOut;
 
 });
-
-//----------------------------------------------------------------------------------------------
-
-let myLink4 = document.querySelector("#myLink4");
-
-myLink4.addEventListener("mouseover", function () {
-    myLink4.style.color = "black";
-});
-
-myLink4.addEventListener("mouseout", function () {
-    myLink4.style.color = "rgb(76, 76, 76)";
-});
-
 //----------------------------------------------------------------------------------------------
 
 let header = document.querySelector("header");
