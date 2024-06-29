2fa3dc41ad48d22a105f55fd6fdc116146353045
neighborhood-karen <96897935+neighborhood-karen@users.noreply.github.com>
2024-06-29 08:51:14 -0700

    EVERYTHING

---
+++ Pages/script.js
@@ -0,0 +1,60 @@
+let myLink1 = document.querySelector("#myLink1");
+
+mylinksColor = "white";
+myLinksColorOut = "rgb(157, 157, 157)";
+
+myLink1.addEventListener("mouseover", function () {
+    myLink1.style.color = mylinksColor;
+    });
+    
+myLink1.addEventListener("mouseout", function () {
+    myLink1.style.color = myLinksColorOut;
+    });
+        
+//----------------------------------------------------------------------------------------------
+
+let myLink2 = document.querySelector("#myLink2");
+
+        
+myLink2.addEventListener("mouseover", function () {
+    myLink2.style.color = mylinksColor;
+});
+
+myLink2.addEventListener("mouseout", function () {
+    myLink2.style.color = myLinksColorOut;
+
+});
+
+//----------------------------------------------------------------------------------------------
+
+let myLink3 = document.querySelector("#myLink3");
+
+myLink3.addEventListener("mouseover", function () {
+    myLink3.style.color =  mylinksColor;
+});
+
+myLink3.addEventListener("mouseout", function () {
+    myLink3.style.color = myLinksColorOut;
+
+});
+//----------------------------------------------------------------------------------------------
+
+let header = document.querySelector("header");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+    let scrollThreshold = window.innerHeight * 0.03;
+    let myName = document.querySelector(".myName");
+    if (document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold) {
+      header.style.paddingTop = "0vw";
+      header.style.paddingBottom = "0vw";
+      myName.style.paddingTop = "6vw";
+    } else {
+      header.style.paddingTop = "1vw";
+      header.style.paddingBottom = "1vw";
+      myName.style.paddingTop = "5";
+    }
+}
+// MAKE IT SO THE HEADER LINKS GET MOVED CLOSER TO THE TITLE WHEN YOU SCROLL DOWN
